4653ed66bda6aa3d4c66eff3fdd8a1aba22d6bc8
NIIDOME Hiromasa <niidome@urb.co.jp>
2022-11-21 17:12:29 +0900

    列名を変更する

--- 103-graph-circle-example.py
+++ 103-graph-circle-example.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 
 df = pd.read_excel("data/child.xlsx", index_col=0)
+df = df.rename(columns={"％": "子供の人数"})
 # print(df)
 
 color_list = ["blue", "green", "yellow", "orange", "red"]
-df["％"].plot.pie(colors=color_list)
+df["子供の人数"].plot.pie(colors=color_list)
 
-plt.title("子供の人数")
 plt.show()

623ebb8366ec450370022b358bac38f8878d6df9
NIIDOME Hiromasa <niidome@urb.co.jp>
2022-11-21 17:05:00 +0900

    色とタイトルを指定

--- 103-graph-circle-example.py
+++ 103-graph-circle-example.py
@@ -5,5 +5,8 @@ import japanize_matplotlib
 df = pd.read_excel("data/child.xlsx", index_col=0)
 # print(df)
 
-df["％"].plot.pie()
+color_list = ["blue", "green", "yellow", "orange", "red"]
+df["％"].plot.pie(colors=color_list)
+
+plt.title("子供の人数")
 plt.show()

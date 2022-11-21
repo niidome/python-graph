edbba510bdce762956fab363f46970337d46e073
NIIDOME Hiromasa <niidome@urb.co.jp>
2022-11-21 16:51:17 +0900

    色を指定

--- 102-graph-line-example.py
+++ 102-graph-line-example.py
@@ -5,8 +5,8 @@ import japanize_matplotlib
 df = pd.read_csv("data/temperature.csv", index_col=0, encoding="shift_jis")
 # print(df)
 
-df["最低気温"].plot()
-df["最高気温"].plot()
+df["最高気温"].plot(color="red")
+df["最低気温"].plot(color="blue")
 
 plt.legend(loc="lower right")
 plt.show()
